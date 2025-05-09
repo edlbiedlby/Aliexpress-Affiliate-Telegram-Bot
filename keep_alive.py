6f6e0d3ef884b9553cdab58392bf707a23546208
edlbiedlby <sis.billard77777@gmail.com>
2025-05-09 14:24:26 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,65 +1,18 @@
-# keep_alive.py
-
-
-
 from flask import Flask
-
 from threading import Thread
 
-import requests
-
-import time
-
-
-
-app = Flask(__name__)
-
+app = Flask('')
 
 
 @app.route('/')
-
 def home():
-
-    return "?"
-
+  return "I'm alive"
 
 
 def run():
-
-    app.run(host='0.0.0.0', port=8080)
-
+  app.run(host='0.0.0.0', port=8080)
 
 
 def keep_alive():
-
-    t = Thread(target=run)
-
-    t.start()
-
-
-
-def self_ping():
-
-    while True:
-
-        try:
-
-            requests.get("https://aliexpress-affiliate-telegram-bot-ju24.onrender.com")  
-
-            print(" hello world ")
-
-        except:
-
-            print("fail ping")
-
-        time.sleep(60 * 3)
-
-		
-
-		
-
-		
-
-keep_alive()
-
-Thread(target=self_ping).start()		
+  t = Thread(target=run)
+  t.start()

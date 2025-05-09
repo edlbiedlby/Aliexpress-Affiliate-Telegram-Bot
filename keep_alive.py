deb9617966076d165514ed3bc308d01c2e1b693b
edlbiedlby <sis.billard77777@gmail.com>
2025-05-09 13:48:21 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -44,7 +44,7 @@ def self_ping():
 
         try:
 
-            requests.get("https://aliexpress-telegram-bot-qjdv.onrender.com")  
+            requests.get("https://aliexpress-affiliate-telegram-bot-ju24.onrender.com")  
 
             print(" hello world ")
 

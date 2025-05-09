92f2f490cc5c74dc6945753742f86b1f93563e30
edlbiedlby <sis.billard77777@gmail.com>
2025-05-09 22:20:59 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "Je suis vivant !"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)  # Change le port à 5000, ou un autre port non utilisé.
 
 def keep_alive():
     t = Thread(target=run)

066778fa336f449644bffc6ed8363688808b2df4
edlbiedlby <sis.billard77777@gmail.com>
2025-05-09 13:45:37 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,25 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)  # Il est recommandé d'utiliser __name__ pour l'initialisation de Flask.
 
 
 @app.route('/')
 def home():
-  return "I'm alive"
+    return "I'm alive"
 
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+    """
+    Fonction pour démarrer l'application Flask.
+    """
+    app.run(host='0.0.0.0', port=8080)  # Le port peut être modifié selon le besoin.
 
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    """
+    Fonction pour démarrer l'application Flask dans un thread séparé.
+    """
+    t = Thread(target=run)  # Création d'un thread pour exécuter la fonction run() en arrière-plan
+    t.daemon = True  # Définir le thread comme daemon pour qu'il s'arrête lorsque le programme principal se termine.
+    t.start()  # Lancement du thread.
